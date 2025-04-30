4054bc9545759c3d32e135466a3a5500fd71f549
Julio Cesar Soares Avelar <jubuchin4@gmail.com>
2025-04-30 11:46:38 -0300

    Update lpmb_ex051.ino

--- Exercícios/lpmb_ex051.ino
+++ Exercícios/lpmb_ex051.ino
@@ -2,14 +2,14 @@ float centimetros;
 float polegadas;
 
 void setup() {
-  Serial.begin(9600); // Inicializa a comunicação serial
+  Serial.begin(9600); 
   Serial.println("Digite a medida em centímetros:");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    centimetros = Serial.parseFloat(); // Lê o número digitado
-    polegadas = centimetros / 2.54;    // Converter para polegadas
+    centimetros = Serial.parseFloat(); 
+    polegadas = centimetros / 2.54;    
 
     Serial.print("Medida em polegadas: ");
     Serial.println(polegadas);
